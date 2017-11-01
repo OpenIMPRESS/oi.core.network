f16c338ad86ca62ada882e1eca6679ff10e46967
Emiel Harmsen <emielch@gmail.com>
2017-11-01 17:14:02 +0100

    init

---
+++ ConnectorLogger.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace oi.core.network {
+
+    public struct Message {
+        public byte[] data;
+        public float timestamp;
+    }
+
+
+    public class ConnectorLogger : MonoBehaviour {
+
+        private Task _writeTask;
+        private bool _writeRunning = false;
+        private ManualResetEvent write_MRSTE = new ManualResetEvent(false);
+        private Queue<Message> _writeQueue = new Queue<Message>();
+        private string type;
+        private string socketID;
+
+        private float currentTime = 0;
+        private string folderPath;
+
+        public void Init(string path, UDPConnector client, bool dataIn) {
+            folderPath = path;
+            socketID = client.socketID;
+
+            if (dataIn) {
+                client.OnDataIn += NewData;
+                type = "IN";
+            } else {
+                client.OnDataOut += NewData;
+                type = "OUT";
+            }
+
+            _writeTask = Task.Run(() => DataWriter());
+        }
+
+        private void NewData(byte[] data) {
+            Message mes = new Message();
+            mes.data = data;
+            mes.timestamp = currentTime;
+            lock (_writeQueue) {
+                _writeQueue.Enqueue(mes);
+                write_MRSTE.Set();
+                write_MRSTE.Reset();
+            }
+        }
+
+        private async void DataWriter() {
+            _writeRunning = true;
+
+            string path = folderPath + "/" + socketID + "_" + type + ".rec";
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append))) {
+
+                while (_writeRunning) {
+                    write_MRSTE.WaitOne();
+                    //Debug.Log(socketID + " writer Unlocked");
+                    int queueCount = 1;
+
+                    while (queueCount > 0) {
+                        Message nextMes = new Message();
+                        bool gotMes = false;
+                        lock (_writeQueue) {
+                            queueCount = _writeQueue.Count;
+                            if (queueCount > 0) {
+                                nextMes = _writeQueue.Dequeue();
+                                gotMes = true;
+                            }
+                        }
+                        if (gotMes) {
+                            writer.Write(nextMes.timestamp);
+                            writer.Write(nextMes.data.Length);
+                            writer.Write(nextMes.data);
+                            writer.Flush();
+                        }
+                    }
+                }
+                writer.Close();
+
+            }
+        }
+
+        private void OnApplicationQuit() {
+            _writeRunning = false;
+        }
+
+        private void Update() {
+            currentTime = Time.time;
+        }
+
+    }
+
+
+}
---
+++ LogStarter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+namespace oi.core.network {
+
+    public class LogStarter : MonoBehaviour {
+
+        private UDPConnector[] clients;
+        private StreamWriter writer;
+        private string folderPath;
+
+
+        private void Start() {
+            var time = System.DateTime.UtcNow;
+            string timeString = time.Year.ToString("0000") + time.Month.ToString("00") + time.Day.ToString("00") + "_" + time.Hour.ToString("00") + time.Minute.ToString("00") + time.Second.ToString("00");
+
+            folderPath = "Recordings/" + timeString;
+            if (!Directory.Exists(folderPath)) {
+                DirectoryInfo di = Directory.CreateDirectory(folderPath);
+            }
+
+
+            clients = FindObjectsOfType<UDPConnector>();
+            foreach (UDPConnector client in clients) {
+                //if (client.socketID != "kinect1") continue;
+
+                ConnectorLogger newLoggerIn = gameObject.AddComponent<ConnectorLogger>() as ConnectorLogger;
+                newLoggerIn.Init(folderPath, client, true);
+
+                ConnectorLogger newLoggerOut = gameObject.AddComponent<ConnectorLogger>() as ConnectorLogger;
+                newLoggerOut.Init(folderPath, client, false);
+            }
+
+        }
+    }
+}
